08ce474ae15ca943ef581efb156392f2c3ca74ca
svainua <sva.in.ua@gmail.com>
2022-08-07 15:52:46 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+tim.shape("turtle")
+
+def move_forward():
+    tim.forward(10)
+
+
+def move_backwards():
+    tim.backward(10)
+
+
+def turn_left():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+
+
+def turn_right():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+
+
+def clear_drawing():
+    tim.reset()
+
+
+screen.listen()
+screen.onkey(move_forward, "w")
+screen.onkey(move_backwards, "s")
+screen.onkey(turn_left, "a")
+screen.onkey(turn_right, "d")
+screen.onkey(clear_drawing, "c")
+
+
+screen.exitonclick()
